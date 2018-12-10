ff10771f1bd9865f59e07d2788cc82e4d3bc56a1
Gannon Radenberg <gannon1@ksu.edu>
2018-12-10 14:09:24 -0600

    4

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -24,6 +24,79 @@ exports.students_get = function(req, res, next){
   })
 }
 
+exports.student_update = function(req, res) {
+    const query = req.query
+	const studentId = query.id
+	delete query['id']
+
+	Student.findByIdAndUpdate(studentId, query, {new:true})
+	.then(student => {
+		res.json({
+			confirmation: 'success',
+			data: student
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.student_remove = function(req, res) {
+    const query = req.query
+
+	Student.findByIdAndRemove(query.id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: 'Student '+query.id+' successfully removed.'
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.student_id = function(req, res) {
+	const id = req.params.id
+
+	Student.findById(id)
+	.then(student => {
+		res.json({
+			confirmation: 'success',
+			data: student
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'Student ' + id + ' not found.'
+		})
+	})
+}
+
+
+exports.student_post = function(req, res) {
+    Student.create(req.body)
+	.then(student => {
+		res.json({
+			confirmation: 'success',
+			data: student
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
 exports.schools_get = function(req, res, next){
   
   School.find({})
@@ -33,6 +106,80 @@ exports.schools_get = function(req, res, next){
   })
 }
 
+exports.schools_update = function(req, res) {
+    const query = req.query // require: id, key=value
+	const schoolsId = query.id
+	delete query['id']
+
+	School.findByIdAndUpdate(schoolsId, query, {new:true})
+	.then(schools => {
+		res.json({
+			confirmation: 'success',
+			data: schools
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.schools_remove = function(req, res) {
+    const query = req.query
+
+	School.findByIdAndRemove(query.id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: 'School '+query.id+' successfully removed.'
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.student_id = function(req, res) {
+	const id = req.params.id
+
+	School.findById(id)
+	.then(schools => {
+		res.json({
+			confirmation: 'success',
+			data: schools
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'School ' + id + ' not found.'
+		})
+	})
+}
+
+
+exports.schools_post = function(req, res) {
+    Schools.create(req.body)
+	.then(schools => {
+		res.json({
+			confirmation: 'success',
+			data: schools
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+
 exports.topics_get = function(req, res, next){
   
   Topic.find({})
@@ -40,6 +187,79 @@ exports.topics_get = function(req, res, next){
   .then(function(results){
     res.render('admin_topics', {topics: results});
   })
+}
+
+exports.topics_update = function(req, res) {
+    const query = req.query // require: id, key=value
+	const topicId = query.id
+	delete query['id']
+
+	Topic.findByIdAndUpdate(topicId, query, {new:true})
+	.then(topics => {
+		res.json({
+			confirmation: 'success',
+			data: topics
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.topics_remove = function(req, res) {
+    const query = req.query
+
+	Topic.findByIdAndRemove(query.id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: 'Topic '+query.id+' successfully removed.'
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.topics_id = function(req, res) {
+	const id = req.params.id
+
+	Topic.findById(id)
+	.then(topics => {
+		res.json({
+			confirmation: 'success',
+			data: topics
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'Topic ' + id + ' not found.'
+		})
+	})
+}
+
+
+exports.topics_post = function(req, res) {
+    Topic.create(req.body)
+	.then(topics => {
+		res.json({
+			confirmation: 'success',
+			data: topics
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
 }
 
 exports.presenters_get = function(req, res, next){
@@ -51,3 +271,75 @@ exports.presenters_get = function(req, res, next){
   })
 }
 
+exports.presenters_update = function(req, res) {
+    const query = req.query // require: id, key=value
+	const presenterId = query.id
+	delete query['id']
+
+	Student.findByIdAndUpdate(presenterId, query, {new:true})
+	.then(presenters => {
+		res.json({
+			confirmation: 'success',
+			data: presenters
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.student_remove = function(req, res) {
+    const query = req.query
+
+	Presenter.findByIdAndRemove(query.id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: 'Presenter '+query.id+' successfully removed.'
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.presenters_id = function(req, res) {
+	const id = req.params.id
+
+	Presenter.findById(id)
+	.then(presenters => {
+		res.json({
+			confirmation: 'success',
+			data: presenters
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'Presenter ' + id + ' not found.'
+		})
+	})
+}
+
+
+exports.presenters_post = function(req, res) {
+    Presenter.create(req.body)
+	.then(presenters => {
+		res.json({
+			confirmation: 'success',
+			data: presenters
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -27,6 +27,79 @@ exports.topic_list_get = function(req, res, next){
   })
 }
 
+exports.topic_list_update = function(req, res) {
+    const query = req.query
+	const topicId = query.id
+	delete query['id']
+
+	Topic.findByIdAndUpdate(topicId, query, {new:true})
+	.then(topic_list => {
+		res.json({
+			confirmation: 'success',
+			data: topic_list
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.topic_list_remove = function(req, res) {
+    const query = req.query
+
+	Topic.findByIdAndRemove(query.id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: 'Topic '+query.id+' successfully removed.'
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.topic_id = function(req, res) {
+	const id = req.params.id
+
+	Topic.findById(id)
+	.then(topic_list => {
+		res.json({
+			confirmation: 'success',
+			data: topic_list
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'Topic ' + id + ' not found.'
+		})
+	})
+}
+
+
+exports.topic_list_post = function(req, res) {
+    Topic.create(req.body)
+	.then(topic => {
+		res.json({
+			confirmation: 'success',
+			data: topic_list
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
 exports.topic_detail_get = function(req, res, next){
 
   Topic.findById(req.params.id)
@@ -37,4 +110,77 @@ exports.topic_detail_get = function(req, res, next){
       res.render('topic_detail', {topic});
     }
   })
+}
+
+exports.topic_detail_update = function(req, res) {
+    const query = req.query
+	const topicId = query.id
+	delete query['id']
+
+	Topic.findByIdAndUpdate(topicId, query, {new:true})
+	.then(topic_detail => {
+		res.json({
+			confirmation: 'success',
+			data: topic_detail
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.topic_detail_remove = function(req, res) {
+    const query = req.query
+
+	Topic.findByIdAndRemove(query.id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: 'Topic '+query.id+' successfully removed.'
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+}
+
+exports.topic_detail_id = function(req, res) {
+	const id = req.params.id
+
+	Topic.findById(id)
+	.then(topic_detail => {
+		res.json({
+			confirmation: 'success',
+			data: topic_detail
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: 'Topic ' + id + ' not found.'
+		})
+	})
+}
+
+
+exports.topic_detail_post = function(req, res) {
+    Topic.create(req.body)
+	.then(topic_detail => {
+		res.json({
+			confirmation: 'success',
+			data: topic_detail
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
 }
